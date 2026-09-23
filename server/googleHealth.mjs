48713ae96f3acb9e3b206323b4a860a95d7c3d15
bazarovegor3-cloud <bazarovegor3@gmail.com>
2026-09-23 13:37:31 +0300

    Request Google Health recovery metrics

--- server/googleHealth.mjs
+++ server/googleHealth.mjs
@@ -6,6 +6,7 @@ const TOKEN_PATH = 'habbits/google-health-tokens.json'
 export const GOOGLE_HEALTH_SCOPES = [
   'https://www.googleapis.com/auth/googlehealth.activity_and_fitness.readonly',
   'https://www.googleapis.com/auth/googlehealth.sleep.readonly',
+  'https://www.googleapis.com/auth/googlehealth.health_metrics_and_measurements.readonly',
 ]
 
 const requiredEnv = (name) => {
